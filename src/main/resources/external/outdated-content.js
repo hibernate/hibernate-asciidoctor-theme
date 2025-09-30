f3be8a55ae24e54a501bd507137d86a9ecef0484
marko-bekhta <marko.prykladna@gmail.com>
2025-09-30 16:50:47 +0200

    Adjust the outdated-content.js to render the version dropdown

--- src/main/resources/external/outdated-content.js
+++ src/main/resources/external/outdated-content.js
@@ -1,27 +1,83 @@
-var HibernateDoc = HibernateDoc || {};
+let HibernateDoc;
+if (!HibernateDoc) {
+    HibernateDoc = {};
+}
 
-HibernateDoc.OutdatedContent = (function() {
-    var install = function(project) {
+HibernateDoc.OutdatedContent = (function () {
+    const install = function (project, basepath) {
         // sanitize project value
         if (!/[a-z]+/.exec(project)) {
             return;
         }
+        if (!basepath) {
+            basepath = '/hibernate/_outdated-content/';
+        }
 
         // build the url to redirect to
-        jQuery_3_1.getJSON('/hibernate/_outdated-content/' + project + '.json', function (json) {
+        jQuery_3_1.getJSON(basepath + project + '.json', function (json) {
+            const button = document.getElementById('versionSelectButton');
+            const dropdown = document.getElementById('versionSelectList');
+            // Function to render the version list
+            const renderVersions = (list) => {
+                // first figure out the url pattern:
+                // e.g. /hibernate/stable/search/reference/en-US/html_single/
+                const versionPattern = /(\/[\d.]+\/|\/current|\/stable\/)/;
+
+                const currentPath = window.location.pathname;
+                const currentVersionMatch = currentPath.match(versionPattern);
+
+                if (!currentVersionMatch) {
+                    console.warn("Could not find a version ('X.Y', 'current', or 'stable') in the current path. Skipping version dropdown construction.");
+                    button.className ='hidden';
+                    return;
+                }
+
+                const versionSegment = currentVersionMatch[0];
+
+                dropdown.innerHTML = '';
+                list.forEach(version => {
+                    const item = document.createElement('a');
+                    item.textContent = version;
+                    item.className = 'version-dropdown-item';
+                    item.href = currentPath.replace(versionSegment, `/${version}/`);
+                    dropdown.appendChild(item);
+                });
+            };
+
+            // Extract version tags from the response
+            const versions = json.versions.map(v => v.version);
+            renderVersions(versions);
+
+            // Event listener for button click
+            button.addEventListener('click', (e) => {
+                if (dropdown.style.display === 'block') {
+                    dropdown.style.display = 'none';
+                } else {
+                    dropdown.style.display = 'block';
+                }
+                e.stopPropagation();
+            });
+
+            // Hide dropdown when clicking outside
+            document.addEventListener('click', (e) => {
+                if (!e.target.closest('#versionSelect')) {
+                    dropdown.style.display = 'none';
+                }
+            });
+
             // Follow the scroll of the user to choose the best hash possible
-            var currentHash = window.location.hash;
+            let currentHash = window.location.hash;
             jQuery_3_1(document).scroll(function () {
                 // in order: html docbook output, html5 docbook output, asciidoc output
                 jQuery_3_1('h2 > a[id], h3 > a[id], section[id], h2[id], h3[id]').each(function () {
-                    var hash = jQuery_3_1(this).attr('id');
+                    const hash = jQuery_3_1(this).attr('id');
                     if (/^d0e[0-9]+$/.exec(hash)) {
                         // we don't consider the numeric hashes as they have been renamed to strings later
                         return;
                     }
-                    var top = window.pageYOffset;
-                    var distance = top - jQuery_3_1(this).offset().top;
-                    if (distance < 30 && distance > -30 && currentHash != hash) {
+                    const top = window.scrollY;
+                    const distance = top - jQuery_3_1(this).offset().top;
+                    if (distance < 30 && distance > -30 && currentHash !== hash) {
                         if (history.replaceState) {
                             history.replaceState(null, null, "#" + hash);
                         } else {
@@ -32,45 +88,45 @@ HibernateDoc.OutdatedContent = (function() {
                 });
             });
 
-            var currentUrl = window.location.pathname;
-            var stableUrl;
-            var pageHash;
+            const currentUrl = window.location.pathname;
+            let stableUrl;
+            let pageHash;
 
             // multi page
-            var matchMulti = new RegExp(json.multi.pattern).exec(currentUrl);
-            var matchSingle = new RegExp(json.single.pattern).exec(currentUrl);
+            const matchMulti = new RegExp(json.multi.pattern).exec(currentUrl);
+            const matchSingle = new RegExp(json.single.pattern).exec(currentUrl);
 
-            if (matchMulti && matchMulti.length == 3) {
-                var currentVersion = matchMulti[1];
-                var currentPage = matchMulti[2];
+            if (matchMulti && matchMulti.length === 3) {
+                const currentVersion = matchMulti[1];
+                const currentPage = matchMulti[2];
 
-                if (currentVersion == json.stable || _isNewerThan(currentVersion, json.stable) || currentVersion == 'stable' || currentVersion == 'current') {
+                if (currentVersion === json.stable || _isNewerThan(currentVersion, json.stable) || currentVersion === 'stable' || currentVersion === 'current') {
                     return;
                 }
 
-                var redirectToMultiPage = (json.multi.target.indexOf('${page}') > -1);
+                const redirectToMultiPage = (json.multi.target.indexOf('${page}') > -1);
 
                 if (redirectToMultiPage) {
-                    var i = 0;
-                    var redirectStart = 0;
-                    var redirectEnd = 0;
-                    for (versionInformation of json.versions) {
-                        if (versionInformation.version == json.stable) {
+                    let i = 0;
+                    let redirectStart = 0;
+                    let redirectEnd = 0;
+                    for (let versionInformation of json.versions) {
+                        if (versionInformation.version === json.stable) {
                             redirectEnd = i;
                         }
-                        if (versionInformation.version == currentVersion) {
+                        if (versionInformation.version === currentVersion) {
                             redirectStart = i;
                             break;
                         }
                         i++;
                     }
 
-                    if (redirectStart == 0) {
+                    if (redirectStart === 0) {
                         return;
                     }
 
-                    var redirectPage = currentPage;
-                    for (var j = redirectStart; j >= redirectEnd; j--) {
+                    let redirectPage = currentPage;
+                    for (let j = redirectStart; j >= redirectEnd; j--) {
                         if (!('redirects' in json.versions[j])) {
                             continue;
                         }
@@ -85,8 +141,8 @@ HibernateDoc.OutdatedContent = (function() {
                     stableUrl = json.multi.target.replace('${version}', json.stable);
                 }
             } else if (matchSingle && matchSingle.length > 2) {
-                var currentVersion = matchSingle[1];
-                if (currentVersion == json.stable || _isNewerThan(currentVersion, json.stable) || currentVersion == 'stable' || currentVersion == 'current') {
+                const currentVersion = matchSingle[1];
+                if (currentVersion === json.stable || _isNewerThan(currentVersion, json.stable) || currentVersion === 'stable' || currentVersion === 'current') {
                     return;
                 }
 
@@ -112,9 +168,8 @@ HibernateDoc.OutdatedContent = (function() {
                 '	text-align:center;' +
                 '	width:100%;' +
                 '	padding: 20px;' +
-                '	background-color: orange;' +
                 '	background-color: #ffbc3b;' +
-                '	border-top: 1px solid orange;' +
+                '	border-top: 1px solid #ffbc3b;' +
                 '	font-weight: bold;' +
                 '	font-size: 20px;' +
                 '	color: white;' +
@@ -145,45 +200,45 @@ HibernateDoc.OutdatedContent = (function() {
                 '	margin-bottom: 8em;' +
                 '}' +
                 '</style>');
-            if (document.cookie.indexOf('hibernate-doc-hide-outdated-cookie=true') == -1) {
-                jQuery_3_1('body').append('<div class="outdated-content">This content refers to an earlier version of ' + json.project + '. Go to latest stable: <a id="stable-url" href="' + stableUrl + '" class="version">version ' + json.stable +'</a>.<a id="close-outdated" title="Close this banner">&times;</a></div>');
-                jQuery_3_1('a#stable-url').on('click', function(e) {
+            if (document.cookie.indexOf('hibernate-doc-hide-outdated-cookie=true') === -1) {
+                jQuery_3_1('body').append('<div class="outdated-content">This content refers to an earlier version of ' + json.project + '. Go to latest stable: <a id="stable-url" href="' + stableUrl + '" class="version">version ' + json.stable + '</a>.<a id="close-outdated" title="Close this banner">&times;</a></div>');
+                jQuery_3_1('a#stable-url').on('click', function (e) {
                     e.preventDefault();
-                    var url = this.href;
-                    if (window.location.hash != '' && window.location.hash != '#' && !/^#d0e[0-9]+$/.exec(window.location.hash)) {
+                    let url = this.href;
+                    if (window.location.hash !== '' && window.location.hash !== '#' && !/^#d0e[0-9]+$/.exec(window.location.hash)) {
                         url += window.location.hash;
                     } else if (pageHash) {
                         url += '#' + pageHash;
                     }
                     window.location.href = url;
                 });
-                jQuery_3_1('a#close-outdated').on('click', function(e) {
+                jQuery_3_1('a#close-outdated').on('click', function (e) {
                     e.preventDefault();
                     jQuery_3_1('.outdated-content').hide();
                     document.cookie = 'hibernate-doc-hide-outdated-cookie=true; path=/';
                 });
             }
-        })
+        });
     }
 
-    var _isNewerThan = function(version, reference) {
-        var versionObject = _extractVersion(version);
-        var referenceObject = _extractVersion(reference);
-        if (versionObject.major == referenceObject.major) {
+    const _isNewerThan = function (version, reference) {
+        const versionObject = _extractVersion(version);
+        const referenceObject = _extractVersion(reference);
+        if (versionObject.major === referenceObject.major) {
             return versionObject.minor > referenceObject.minor;
         }
         return versionObject.major > referenceObject.major;
     }
 
-    var _extractVersion = function(version) {
-        var match = /([0-9]+)\.([0-9]+)/.exec(version);
-        if (match.length != 3) {
+    const _extractVersion = function (version) {
+        const match = /([0-9]+)\.([0-9]+)/.exec(version);
+        if (match.length !== 3) {
             return null;
         }
-        return { major: parseInt(match[1]), minor: parseInt(match[2]) };
+        return {major: parseInt(match[1]), minor: parseInt(match[2])};
     }
 
     return {
         install: install
     }
-}) ();
+})();
